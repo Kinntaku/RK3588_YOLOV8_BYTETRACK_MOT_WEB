1a0647b3f9cdc7ed26bb87a12077ca1857253d10
Kinntaku <suu_kinntaku@outlook.com>
2025-08-22 21:05:27 +0800

    更新enviroments

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # RK3588_YOLOV8_BYTETRACK_MOT_WEB
 A MOT project using YOLOV8 as the detection head and bytetrack as the tracker, deployed on RK3588S, with a web front-end built on Flask.
+
+一个使用yolov8作为检测头，bytetrack作为追踪器，flask作为web前端的多目标追踪项目
+
+---
+
+## 演示视频
+
+见 `project_demo.mp4`
+
+## 界面&功能介绍
+
+- 控件介绍
+	![image-20250822203816581](./README.assets/layout.png)
+
+- 遮罩模式（追踪框外变暗）
+
+	![image-20250822204004994](./README.assets/mask_mode.png)
+
+- 指定id追踪模式
+	![image-20250822204153774](./README.assets/specific_id_track.png)
+
+## 使用方法
+- config.py
+	OBJ_THRESH, NMS_THRESH：置信度，nms阈值
+	IMG_SIZE：模型的输入大小
+	TARGET_CLASSES：想要追踪的类别（以集合形式存储），其类别必须包在CLASSES中
+	CLASSES：模型所支持的所有类别
+	model：想使用的模型
+- 运行方法
+	创建新的conda环境，python=3.10
+	安装 `environment` 文件夹下 `rknn_toolkit_lite2-2.0.0b0-cp310-cp310-linux_aarch64.whl` 包
+	（`rknn_toolkit2-2.0.0b0+9bab5682-cp310-cp310-linux_x86_64.whl` 包为yolo模型转换rknn时使用的包）
+	将 `librknnrt.so` 复制（覆盖）到 `/usr/lib/` 目录下
+	执行 `pip install -r requirements.txt`
+	运行 `python main_cam.py`
+	在同一局域网下使用浏览器访问开发板的5000端口
+
+## 训练日志
+
+![image-20250822201959110](./README.assets/mAP50-95.png)
+
+![image-20250822202113622](./README.assets/mAP50.png)
+
+![image-20250822202148862](./README.assets/recall.png)
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 OBJ_THRESH, NMS_THRESH, IMG_SIZE = 0.5, 0.9,1024
 
 
-TARGET_CLASSES ={"person",} #填写自己需要追踪的类别
+TARGET_CLASSES ={"person","dog","cat"} #填写自己需要追踪的类别
 CLASSES = ("person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light",
            "fire hydrant", "stop sign ", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant",
            "bear", "zebra ", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
@@ -9,9 +9,9 @@ CLASSES = ("person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train",
            "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa",
            "pottedplant", "bed", "diningtable", "toilet ", "tvmonitor", "laptop	", "mouse", "remote", "keyboard", "cell phone", "microwave ",
            "oven ", "toaster", "sink", "refrigerator ", "book", "clock", "vase", "scissors ", "teddy bear ", "hair drier", "toothbrush ")
-TARGET_CLASSES ={"person","dog","cat"}
+
 
 model = "./rknnModel/ch_200_ReLU_best_1024.rknn" #使用crowdhuman数据集训练200次的1024输入大小，relu作为激活函数的模型
 #./rknnModel/ch_best_180_1024.rknn ：使用crowdhuman训练的1024输入大小的模型，
-#./rknnModel/ch_best_180_640.rknn ：使用crowdhuman训练的640输入大小的模型，
-#./rknnModel/yolov8s.rknn ：yolov8s默认模型
+#./rknnModel/ch_best_180_640.rknn ：使用crowdhuman训练的640输入大小的模型（使用时记得修改IMG_SIZE），
+#./rknnModel/yolov8s.rknn ：yolov8s默认模型（使用时记得修改IMG_SIZE）
